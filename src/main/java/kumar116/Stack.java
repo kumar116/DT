f3e91c2ec20db499a95a8f7c5dd22acfe96cdead
Soumya Kumar <soumyakumar85@gmail.com>
2017-03-14 02:46:03 -0400

    Use extends in Stack

--- src/main/java/kumar116/Stack.java
+++ src/main/java/kumar116/Stack.java
@@ -1,29 +1,17 @@
 package kumar116;
 
-public class Stack<T> {
-
-    private LinkedList<T> list;
-    public Stack() {
-        list = new LinkedList<T>();
-    }
+public class Stack<T> extends LinkedList<T> {
 
     public void push(T data) {
-        list.addToHead(data);
+        addToHead(data);
     }
 
     public T pop() {
-        return list.popFromHead();
-    }
-
-    public int size() {
-        return list.size;
+        return popFromHead();
     }
 
     public boolean isEmpty() {
-      return list.size == 0;
+      return size == 0;
     }
 
-    public String traverse() {
-      return list.traverse();
-    }
 }
